1c037643a9190b222e03d8522c81112ccebf6277
Michael Conrad <michael@threepears.com>
2016-03-10 23:03:08 -0600

    Fixing problem with scope updating upon new entries

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,54 +1,97 @@
 angular.module('starter.controllers', [])
 
-.controller('MainCtrl', function($scope) {
+.controller('MainCtrl', function($scope, $rootScope) {
 
   // Retrieve existing grocery list, if any, from local storage
-  let storedItems = JSON.parse(localStorage.getItem("groceries"));
+
+  $rootScope.storedItems = JSON.parse(localStorage.getItem("groceries")) || [];
 
 
   // Set scope variable with local storage list
-  $scope.groceryArray = storedItems;
+  // $scope.groceryArray = $scope.storedItems || [];
 
 
   // Add grocery items to list, both in local storage and scope
-  $scope.getGroceries = function() {
+  $scope.getGroceries = function(spokenGroc) {
+    var localItems = $rootScope.storedItems;
 
-    let groc = document.getElementById("groceries").value;
+    // let groc = spokenGroc;
+    // document.getElementById("groceries").value;
+    console.log(spokenGroc);
 
-    if (groc === "") {
+    if (spokenGroc === "") {
       return false;
     }
 
-    console.log(groc);
+    console.log(spokenGroc);
 
-    let grocItems = groc.split('next');
+    let grocItems = spokenGroc.split('next');
 
     grocItems = grocItems.map( e => {return e.trim();} );
 
     console.log(grocItems);
 
-    if (storedItems) {
+    if (localItems) {
       for (let i = 0; i < grocItems.length; i++) {
-        if (storedItems.indexOf(grocItems[i] !== -1)) {
-          storedItems.push(grocItems[i]);
+        if (localItems.indexOf(grocItems[i] !== -1)) {
+          localItems.push(grocItems[i]);
+          console.log(localItems);
         }
       }
     } else {
-      storedItems = grocItems;
+      localItems = grocItems;
     }
 
-    localStorage.setItem("groceries", JSON.stringify(storedItems));
-    $scope.groceryArray = storedItems;
+    localStorage.setItem("groceries", JSON.stringify(localItems));
 
-    console.log(storedItems);
+    console.log(localItems);
+    $rootScope.storedItems = localItems;
+    // console.log($scope.groceryArray);
+
+    // $scope.groceryArray.push($scope.storedItems);
+    $scope.$digest();
+
+  };
+
+
+  // HTML5 Speech Recognition API
+  $scope.startDictation = function() {
+
+    if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+      var recognition = new webkitSpeechRecognition();
+
+      recognition.continuous = false;
+      recognition.interimResults = false;
+
+      recognition.lang = "en-US";
+      recognition.start();
+
+      recognition.onresult = function(e) {
+        // document.getElementById('transcript').value
+        var spokenGroc = e.results[0][0].transcript;
+        console.log(spokenGroc);
+        $scope.getGroceries(spokenGroc);
+        recognition.stop();
+        // document.getElementById('labnol').submit();
+      };
+
+      recognition.onerror = function(e) {
+        recognition.stop();
+      }
+    }
   };
 
 
   // Delete item from page and local storage
   $scope.deleteItem = function(item) {
-    let position = storedItems.indexOf(item);
-    storedItems.splice(position, 1);
-    localStorage.setItem("groceries", JSON.stringify(storedItems));
+    console.log(item);
+    let position = $rootScope.storedItems.indexOf(item);
+    console.log($rootScope.storedItems);
+    $rootScope.storedItems.splice(position, 1);
+    console.log($rootScope.storedItems);
+    localStorage.setItem("groceries", JSON.stringify($rootScope.storedItems));
+    console.log($rootScope.storedItems);
   };
 
 })
